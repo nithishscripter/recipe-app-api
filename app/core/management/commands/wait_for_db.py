0b2f1e06ea14bc3a9c4a4be03184ff699c0dfafb
Nithish <nithish1326@gmail.com>
2025-07-08 21:37:51 +0530

    Configured docker compose and actions to wait for db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,10 +1,28 @@
 """Custom management command to wait for the Data Base to be available."""
 
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+import time
 
 
 class Command(BaseCommand):
     """Django command to wait for db."""
 
     def handle(self, *args, **options):
-        pass
+        """Entrypoint for Command."""
+
+        db_up = False
+        self.stdout.write("Preparing Database...")
+
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(OperationalError, Psycopg2OpError):
+                self.stdout.write("Database is unavailable, re-trying...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database AVAILABLE!"))
